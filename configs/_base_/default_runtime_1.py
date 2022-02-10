bc09703bbae63e90dc39523c9968430f4483756a
vampireacen <67774064+vampireacen@users.noreply.github.com>
2022-02-11 00:47:38 +0800

    add configs

---
+++ configs/_base_/default_runtime_1.py
@@ -0,0 +1,17 @@
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=1,
+    hooks=[
+        # dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+        dict(type='WandbLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = 'mask_rcnn_swin_1.pth'
+resume_from = None
+workflow = [('train', 1)]
